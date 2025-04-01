3ac3f58e9e2e0ae2c86d395ac15b978f969643e3
Santiago3102 <santiago.gomez3@utp.edu.co>
2025-03-31 21:51:04 -0500

    Ejercicios taller de agrupaciones trabajados en clase

--- Script-8.sql
+++ Script-8.sql
@@ -1,7 +1,21 @@
+/*
+ * 1. Usando el comando DISTINCT (consultar por internet como funciona) mostrar
+ * solo la lista de propiedades que han sido visitadas.
+ * 2. ¿Cuáles son las personas con el sueldo más alto y el más bajo?
+ * 3. ¿Cuanto se debe pagar laboralmente por cada sucursal?
+ * 4. ¿Cuál es el máximo, mínimo y el promedio del salario de los empleados?
+ * 5. Mostrar por cada sucursal, por cada cargo y por el sexo, el número de personas
+ * y el pago total del salario.
+ */
+
+ /*
+ * 6. Cuál es el cliente que más ha visitado propiedades para alquiler? 
+ */
+
 select c.clientno, c.fname, c.lname
 from client c 
 join viewing v on c.clientno = v.clientno
-group by c.clientno 
+group by c.clientno, c.fname, c.lname 
 having count(*) = (select max(contar)
                          from( select clientno, count(clientno) as contar 
                          from viewing 
@@ -68,6 +82,56 @@ select b.*, (select count(*) as empleados_sucursal
 from branch b
 
 
+select 
+from
+(select v.propertyno, count(p.propertyno ) as propiedades_mas_visitada
+from viewing v 
+join 
+propertyforrent p on p.propertyno = v.propertyno
+group by v.propertyno) t1 
+full outer join
+(select p.city, count(p.city)
+from propertyforrent p 
+group by p.city) t2
+
+/*
+ * 7. ¿Cuál es la ubicación y la propiedad más visita para arrendar?
+ */
+
+select p.propertyno, p.street, count(*) as visitas_propiedad
+from propertyforrent p 
+join viewing v on p.propertyno  = v.propertyno 
+group by p.propertyno, p.street  
+having count(*) = (select max(contar)
+                         from ( select propertyno, count(propertyno) as contar 
+                         from (select propertyno 
+                               from viewing v2
+                               except 
+                               select propertyno 
+                               from lease l 
+                               ) 
+                         group by propertyno ) 
+                         )
+                         
+/*
+ * 8. ¿Cuál es la persona que más propiedades tiene a su cargo para alquilar?
+ */
 
 
+select s.staffno, s.fname, s.lname 
+from staff s 
+join propertyforrent p  on s.staffno = p.staffno
+group by s.staffno, s.fname, s.lname
+having count(*) = (select max(contar)
+                         from ( select s3.staffno , count(*) as contar 
+                         from (select s2.staffno 
+                               from staff s2
+                               join propertyforrent p2 on s2.staffno = p2.staffno ) s3  
+                         group by s3.staffno ) 
+                         )        
 
+                         
+select s.staffno , count(*)
+from staff s 
+join propertyforrent p on p.staffno = s.staffno
+group by s.staffno 
